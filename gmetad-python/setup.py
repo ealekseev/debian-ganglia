35e9e42d578121979d908732f14bdc956ded18fa
Daniel Pocock <daniel@pocock.com.au>
2013-05-27 16:24:37 +0200

    Imported Upstream version 3.5.0

--- gmetad-python/setup.py
+++ gmetad-python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name='gmetad-python',
-      version='3.4.0',
+      version='3.5.0',
       description='Ganglia Meta daemon in Python',
       maintainer='Ganglia Development Team',
       maintainer_email='ganglia-developers@lists.sourceforge.net',
